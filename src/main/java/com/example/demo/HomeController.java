8eefeb2bc6b7d324c928cb692db3367855e5f7a0
Kaan Sarkim <r0782626@student.thomasmore.be>
2020-05-06 18:17:29 +0200

    test 3 commit naar master

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -1,6 +1,5 @@
 package com.example.demo;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,8 +8,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HomeController {
 
 
-    @Autowired
-    private VideoRepository videoRepository;
+//    @Autowired
+//    private VideoRepository videoRepository;
 
     private Video[] videos= {
 
