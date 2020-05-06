3a87074896f31fe31ae9bc966832a1d6de9ee012
Boterham Met Kaas <r0806377@student.thomasmore.be>
2020-05-06 18:30:03 +0200

    yaaaaa

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -1,17 +1,48 @@
 package com.example.demo;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 
 @Controller
 public class HomeController {
 
+//
+//    @Autowired
+//    private VideoRepository videoRepository;
 
-    @Autowired
-    private VideoRepository videoRepository;
+    private Video[] videos = {
+
+            new Video(0,"Waarom spuitje"),
+            new Video(1,"Waarom wenen"),
+            new Video(2,"Waarom spelen"),
+
+    };
 
 
+    @GetMapping("/")
+    public String VideoPagina(Model model) {
+        model.addAttribute("video", videos);
+        return "VideoPagina";
+    }
+
+
+
+
+
+
+//    @GetMapping("/VideoPagina")
+//    public String VideoPagina(Model model){
+//        model.addAttribute("videos", videoRepository.findAll());
+//        return "VideoPagina";
+//    }
+
+    @GetMapping("/Game/Mol")
+        public String GamePagina(Model model){
+        model.addAttribute("games", games);
+        return "SpelDemol";
+    }
 
 
 }
