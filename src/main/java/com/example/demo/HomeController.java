b9c43b57c313b0720d123ffa5300ac40f479d81a
Gabriel Dela Peña <r0744951@student.thomasmore.be>
2020-05-06 11:39:42 +0200

    InlogPagina Gabriel Progressing

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -1,6 +1,8 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 
 @Controller
@@ -9,9 +11,11 @@ public class HomeController {
 //
 //    @Autowired
 //    private VideoRepository videoRepository;
-
-
-//        @GetMapping
+//
+//
+//        @GetMapping{
+//
+//        }
 
 
 }
