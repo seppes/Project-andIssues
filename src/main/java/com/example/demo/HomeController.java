e2c641c21f2c4dde0517a819a78aee813fd33a17
Gabriel Dela Peña <r0744951@student.thomasmore.be>
2020-05-07 21:42:47 +0200

    InlogPagina Commit

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -1,45 +1,72 @@
 package com.example.demo;
 
-import com.example.demo.VideoRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.example.demo.model.Knuffel;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
 
 @Controller
 public class HomeController {
 
 
-    @Autowired
-    private VideoRepository videoRepository;
+//    @Autowired
+//    private VideoRepository videoRepository;
 
-    private Video[] videos = {
+    private Video[] videos= {
 
             new Video(0,"Waarom spuitje"),
             new Video(1,"Waarom wenen"),
-            new Video(2,"Waarom spelen"),
+            new Video(2,"Waarom spelendd"),
 
     };
-
+    private Knuffel[] knuffels = {
+            new Knuffel(0, "De Egel", "€10"),
+            new Knuffel(1, "De Kangoeroe", "€10"),
+            new Knuffel(2, "De Mol", "€10"),
+    };
 
     @GetMapping("/")
-    public String VideoPagina(Model model) {
-        model.addAttribute("video", videos);
-        return "htmlVideoGames/VideoPagina";
-    }
-
-    @GetMapping({"/Deegel"})
-    public String Deegel(Model model) {
+    public String VideoPaginaMol(Model model) {
+        model.addAttribute("videos", videos);
         return "Deegel";
     }
 
+    @GetMapping("/VideoEgel")
+    public String VideoPaginaEgel(Model model) {
+        model.addAttribute("videos", videos);
+        return "htmlVideoGames/VideoPaginaEgel";
+    }
+
+    @GetMapping("/GamePage")
+    public String VideoPaginaKangerou(Model model) {
+        model.addAttribute("videos", videos);
+        return "htmlVideoGames/GamePage";
+    }
+
+    @GetMapping("/webshop")
+    public String index(Model model) {
+        model.addAttribute("knuffels", knuffels);
+        return "htmlWebshop/index";
+    }
+
+    @GetMapping("/shoppingcart")
+    public String shoppingcart(Model model) {
+        model.addAttribute("knuffels", knuffels);
+        return "htmlWebshop/shoppingcart";
+    }
+
+    @GetMapping("/about")
+    public String about(Model model) {
+        return "htmlWebshop/about";
+    }
+
+    @GetMapping("/contact")
+    public String contact(Model model) {
+        return "htmlWebshop/contact";
+    }
 
 
-
-
-
-//    @GetMapping("/VideoPagina")
+//    @GetMapping ("/VideoPagina")
 //    public String VideoPagina(Model model){
 //        model.addAttribute("videos", videoRepository.findAll());
 //        return "VideoPagina";
