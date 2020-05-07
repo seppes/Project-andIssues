5ec271b86c8fc521452556bf01aae92eba7d3b5a
Seppe Buse <r0787714@thomasmore.be>
2020-05-07 12:15:38 +0200

    1 homecontroller

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import com.example.demo.model.Knuffel;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,7 +19,11 @@ public class HomeController {
             new Video(2,"Waarom spelendd"),
 
     };
-
+    private Knuffel[] knuffels = {
+            new Knuffel(0, "De Egel", "€10"),
+            new Knuffel(1, "De Kangoeroe", "€10"),
+            new Knuffel(2, "De Mol", "€10"),
+    };
 
     @GetMapping("/")
     public String VideoPaginaMol(Model model) {
@@ -38,9 +43,21 @@ public class HomeController {
         return "htmlVideoGames/VideoPaginaKangerou";
     }
 
+    @GetMapping("/webshop")
+    public String index(Model model) {
+        model.addAttribute("knuffels", knuffels);
+        return "htmlWebshop/index";
+    }
 
+    @GetMapping("/about")
+    public String about(Model model) {
+        return "htmlWebshop/about";
+    }
 
-
+    @GetMapping("/contact")
+    public String contact(Model model) {
+        return "htmlWebshop/contact";
+    }
 
 
 //    @GetMapping ("/VideoPagina")
