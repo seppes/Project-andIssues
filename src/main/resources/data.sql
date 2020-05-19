b9d7acb5d233b74b564889bd1e4dfd2f17ae4262
Kaan Sarkim <r0782626@student.thomasmore.be>
2020-05-19 10:43:48 +0200

    alles inorde game en video pagina (code)

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -33,11 +33,11 @@ INSERT INTO USER (ID, USERNAME, PASSWORD, ROLE) VALUES
 
 
 INSERT INTO KNUFFEL (ID, ANIMAL_NAME, ANIMAL_PRICE, ANIMAL_PIC, ANIMAL_DESCRIPTION) VALUES
-( 0, 'De Egel ', '€10', 'img/knuffel_egel.png ', 'De egel heeft naalden.');
+( 0, 'De Egel ', '€10', '/img/knuffel_egel.png ', 'De egel heeft naalden.');
 INSERT INTO KNUFFEL (ID, ANIMAL_NAME, ANIMAL_PRICE, ANIMAL_PIC, ANIMAL_DESCRIPTION) VALUES
-( 1, 'De Kangoeroe ', '€10', 'img/knuffel_kangoeroe.png', 'De kangoeroe springt.');
+( 1, 'De Kangoeroe ', '€10', '/img/knuffel_kangoeroe.png', 'De kangoeroe springt.');
 INSERT INTO KNUFFEL (ID, ANIMAL_NAME, ANIMAL_PRICE, ANIMAL_PIC, ANIMAL_DESCRIPTION) VALUES
-( 2, 'De Mol ', '€10', 'img/knuffel_mol.png', 'De mol graaft.');
+( 2, 'De Mol ', '€10', '/img/knuffel_mol.png', 'De mol graaft.');
 
 
 INSERT INTO VIDEO (ID, TITEL, VIDEO_FILE_NAME, KNUFFEL_ID) VALUES
@@ -63,17 +63,17 @@ INSERT INTO VIDEO (ID, TITEL, VIDEO_FILE_NAME, KNUFFEL_ID) VALUES
 
 
 INSERT INTO GAME (ID, PICTURE_GAME, TITEL_GAME, KNUFFEL_ID) VALUES
-(0, 'img/game5.png', 'Zoekspel', 0);
+(0, '/img/game5.png', 'Zoekspel', 0);
 
 INSERT INTO GAME (ID, PICTURE_GAME, TITEL_GAME, KNUFFEL_ID) VALUES
-(1, 'img/game1.jpg', 'Cave Man', 0);
+(1, '/img/game1.jpg', 'Cave Man', 0);
 
 INSERT INTO GAME (ID, PICTURE_GAME, TITEL_GAME, KNUFFEL_ID) VALUES
-(2, 'img/game2.jpg', 'Puzzle game', 1);
+(2, '/img/game2.jpg', 'Puzzle game', 1);
 
 INSERT INTO GAME (ID, PICTURE_GAME, TITEL_GAME, KNUFFEL_ID) VALUES
-(3, 'img/game3.jpg', 'Fun Math', 2);
+(3, '/img/game3.jpg', 'Fun Math', 2);
 
 INSERT INTO GAME (ID, PICTURE_GAME, TITEL_GAME, KNUFFEL_ID) VALUES
-(4, 'img/game4.png', 'Zoeken', 2);
+(4, '/img/game4.png', 'Zoeken', 2);
 
