41e5c47b4eb01c508e029744269713ed064ca168
sammy <sammylaureys@hotmail.com>
2020-05-21 22:02:32 +0200

    een paar beschrijvingen aangepast van de knuffels.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -5,9 +5,9 @@
 
 
 INSERT INTO KNUFFEL (ID, ANIMAL_NAME, ANIMAL_PRICE, ANIMAL_PIC, ANIMAL_DESCRIPTION) VALUES
-( 0, 'De Egel', '€10', '/img/knuffel_egel.png ', 'De egel heeft naalden.');
+( 0, 'De Egel', '€10', '/img/knuffel_egel.png ', 'De egel kan je kind leren om geen bang te hebben van naalden.');
 INSERT INTO KNUFFEL (ID, ANIMAL_NAME, ANIMAL_PRICE, ANIMAL_PIC, ANIMAL_DESCRIPTION) VALUES
-( 1, 'De Kangoeroe', '€10', '/img/knuffel_kangoeroe.png', 'De kangoeroe springt.');
+( 1, 'De Kangoeroe', '€10', '/img/knuffel_kangoeroe.png', 'De kangoeroe maakt je kind blij door altijd vrolijk in het rond te huppelen.');
 INSERT INTO KNUFFEL (ID, ANIMAL_NAME, ANIMAL_PRICE, ANIMAL_PIC, ANIMAL_DESCRIPTION) VALUES
 ( 2, 'De Mol', '€10', '/img/knuffel_mol.png', 'De mol graaft.');
 
