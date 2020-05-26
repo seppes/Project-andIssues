758a8e62aa9ae4af35ae1573d623209bd3416bf8
Gabriel Dela Peña <r0744951@student.thomasmore.be>
2020-05-26 21:16:41 +0200

    appHome users/knuffel af.

--- src/main/java/com/example/demo/controllers/AdminController.java
+++ src/main/java/com/example/demo/controllers/AdminController.java
@@ -1,15 +1,34 @@
 package com.example.demo.controllers;
 
+import ch.qos.logback.classic.util.LogbackMDCAdapter;
+import com.example.demo.model.User;
+import com.example.demo.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.security.Principal;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/admins")
 public class AdminController {
+    @Autowired
+    private UserRepository userRepository;
 
-    @RequestMapping("/Orders")
-    public String admin(Model model) {
+
+    @GetMapping("/Orders")
+    public String appHome(Principal principal, Model model) {
+        Optional<User> optionalUserFromDb = userRepository.findByUsername(principal.getName());
+        if (optionalUserFromDb.isEmpty()) {
+            model.addAttribute("user", "Geen bestellingen");
+        } else {
+            User user = optionalUserFromDb.get();
+            model.addAttribute("user", user);
+
+        }
         return "admins/AdminOrders";
     }
 
