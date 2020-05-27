98673126458a7264fb64e38f9c1dfbffc84bf6ad
Kaan Sarkim <r0782626@student.thomasmore.be>
2020-05-27 21:14:41 +0200

    gecommit naar andere branche

--- src/main/java/com/example/demo/controllers/AdminController.java
+++ src/main/java/com/example/demo/controllers/AdminController.java
@@ -11,8 +11,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Optional;
 
 @Controller
