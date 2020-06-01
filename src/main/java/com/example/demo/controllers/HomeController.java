a9d0f32cf0b340dc828e8b8320dd90dba59b6815
Seppe Buse <r0787714@thomasmore.be>
2020-06-01 14:10:55 +0200

    spacing homecontroller

--- src/main/java/com/example/demo/controllers/HomeController.java
+++ src/main/java/com/example/demo/controllers/HomeController.java
@@ -25,7 +25,7 @@ public class HomeController {
     private String applicationName = "Knùs";
 
     @Autowired
-   private VideoRepository videoRepository;
+    private VideoRepository videoRepository;
 
     @Autowired
     private KnuffelRepository knuffelRepository;
@@ -34,22 +34,20 @@ public class HomeController {
     private GameRepository gameRepository;
 
 
-
     @GetMapping({"/VideoPage/{knuffelId}"})
-  public String VideoPagina(@PathVariable int knuffelId, Model model) {
-      Optional<Knuffel> optionalKnuffelFromDb = knuffelRepository.findById(knuffelId);
+    public String VideoPagina(@PathVariable int knuffelId, Model model) {
+        Optional<Knuffel> optionalKnuffelFromDb = knuffelRepository.findById(knuffelId);
 
-     if (optionalKnuffelFromDb.isEmpty()) {
-       model.addAttribute("video", new Video[]{});
-   } else {
-         Knuffel knuffel = optionalKnuffelFromDb.get();
-        model.addAttribute("knuffel", knuffel);
-        model.addAttribute("video", videoRepository.findVideosByKnuffel(knuffel));
+        if (optionalKnuffelFromDb.isEmpty()) {
+            model.addAttribute("video", new Video[]{});
+        } else {
+            Knuffel knuffel = optionalKnuffelFromDb.get();
+            model.addAttribute("knuffel", knuffel);
+            model.addAttribute("video", videoRepository.findVideosByKnuffel(knuffel));
 
-     }
-      return "htmlVideoGames/VideoPage";
-      }
-
+        }
+        return "htmlVideoGames/VideoPage";
+    }
 
     @GetMapping({"/GamePage/{knuffelId}"})
     public String GamePagina(@PathVariable int knuffelId, Model model) {
@@ -65,7 +63,6 @@ public class HomeController {
         return "htmlVideoGames/GamePage";
     }
 
-
     @GetMapping("/")
     public String index(Model model) {
         Iterable<Knuffel> knuffelsFromDb = knuffelRepository.findAll();
@@ -73,7 +70,6 @@ public class HomeController {
         model.addAttribute("appName", applicationName);
         return "htmlWebshop/index";
     }
-
 
     @GetMapping("/shoppingcart")
     public String shoppingcart(Model model) {
@@ -95,8 +91,4 @@ public class HomeController {
         model.addAttribute("appName", applicationName);
         return "htmlWebshop/contact";
     }
-
-
-
-
 }
