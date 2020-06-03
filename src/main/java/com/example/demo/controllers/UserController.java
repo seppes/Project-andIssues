0fa782dbd15e18a66962cbf9f2d2d912594b8a4c
Gabriel Dela Peña <r0744951@student.thomasmore.be>
2020-06-03 12:14:29 +0200

    KnuffelId in de database steken. Admin addUser.

--- src/main/java/com/example/demo/controllers/UserController.java
+++ src/main/java/com/example/demo/controllers/UserController.java
@@ -89,26 +89,6 @@ public class UserController {
     }
 
 
-//
-//    @GetMapping("/appHome")
-//    public String appHome(Model model) {
-//        return "htmlHome/appHome";
-//    }
-
-//    @GetMapping({"/appHome/{knuffelId}"})
-//    public String appHome(@PathVariable int knuffelId, Model model) {
-//        Optional<Knuffel> optionalKnuffelFromDb = knuffelRepository.findById(knuffelId);
-//        if (optionalKnuffelFromDb.isEmpty()) {
-//            model.addAttribute("user", new User[]{});
-//        } else {
-//            Knuffel knuffel = optionalKnuffelFromDb.get();
-//            model.addAttribute("knuffel", knuffel);
-//            model.addAttribute("user", userRepository.findUsersByKnuffel(knuffel));
-//
-//        }
-//        return "htmlHome/appHome";
-//    }
-
     @GetMapping("/appHome")
     public String appHome(Principal principal, Model model) {
         Optional<User> optionalUserFromDb = userRepository.findByUsername(principal.getName());//Zoek de username op in de database
